bbfa47c41d488a02d87ed1a8f48ab704e436fe44
Yasaswini Tiramdas <yasaswinipandu@gmail.com>
2024-11-17 00:23:43 -0500

    add pbft

--- transaction.py
+++
@@ -1,8 +0,0 @@
-class Transaction:
-    def __init__(self, sender, receiver, amount):
-        self.sender = sender
-        self.receiver = receiver
-        self.amount = amount
-
-    def __str__(self):
-        return f"{self.sender} -> {self.receiver} : {self.amount}"
